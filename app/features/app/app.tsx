9380c26a70364e98d3ae92d5c95520c4ed7b0395
Ilya Kurganskiy <kurganskiy@mercurydevelopment.com>
2021-09-19 11:18:30 +0400

    build: add react native navigation, add mobx

---
+++ app/features/app/app.tsx
@@ -0,0 +1,6 @@
+import {NavigationContainer} from "@react-navigation/native"
+import * as React from "react"
+
+export default function App() {
+  return <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+}
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 /**
  * @format
  */
+import "react-native-gesture-handler"
+// eslint-disable-next-line import/order
+import {AppRegistry} from "react-native"
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import {name as appName} from "./app.json"
+import App from "./app/features/app/app"
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => App)
